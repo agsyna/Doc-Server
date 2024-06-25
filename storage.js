a0fba570147d9391e45493cbe2675128de2885a2
agsyna <agarwalasyna@gmail.com>
2024-06-25 12:07:00 +0530

    sd

--- storage.js
+++ storage.js
@@ -3,7 +3,7 @@ let path = require('path');
 
 const getDestination = (req, file, cb) => {
   // const folderName = path.join(__dirname, '/'+req.session.type+req.session.pg);
-  const folderName = path.join(__dirname, '/deptfolders/');
+  const folderName = path.join(__dirname, '/deptfolders/1/');
   console.log(folderName);
   console.log(__dirname);
   cb(null, `${folderName}`);
